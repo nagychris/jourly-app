d978ee277d004072db7fe6ea57232f907f348407
Christopher Nagy <chrisn4gy@gmail.com>
2022-12-16 20:55:35 +0100

    feat: remove db instantiation from MainActivity.kt

--- app/src/main/java/com/example/jourlyapp/MainActivity.kt
+++ app/src/main/java/com/example/jourlyapp/MainActivity.kt
@@ -10,17 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.RoomDatabase
-import com.example.jourlyapp.model.journal.db.AppDatabase
 import com.example.jourlyapp.ui.theme.JourlyAppTheme
 
 class MainActivity : ComponentActivity() {
-    private var db: RoomDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        db = AppDatabase.getInstance(applicationContext)
 
         setContent {
             JourlyAppTheme {
