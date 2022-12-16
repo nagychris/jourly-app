d98b25e5ef6280f6d0133b194c21bd42c6526bb2
Christopher Nagy <chrisn4gy@gmail.com>
2022-12-16 20:54:34 +0100

    fix: use LocalDateTime instead of Date

--- app/src/main/java/com/example/jourlyapp/model/journal/db/Converters.kt
+++ app/src/main/java/com/example/jourlyapp/model/journal/db/Converters.kt
@@ -1,18 +1,23 @@
 package com.example.jourlyapp.model.journal.db
 
 import androidx.room.TypeConverter
-import java.util.Date
+import java.time.LocalDateTime
 
 // Provides type converters for storing complex objects (e.g. Date) in the Room DB
 // https://developer.android.com/training/data-storage/room/referencing-data#type-converters
 class Converters {
+
     @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return value?.let { Date(it) }
+    fun toDate(dateString: String?): LocalDateTime? {
+        return if (dateString == null) {
+            null
+        } else {
+            LocalDateTime.parse(dateString)
+        }
     }
 
     @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time?.toLong()
+    fun toDateString(date: LocalDateTime?): String? {
+        return date?.toString()
     }
 }
--- app/src/main/java/com/example/jourlyapp/model/journal/entities/JournalEntry.kt
+++ app/src/main/java/com/example/jourlyapp/model/journal/entities/JournalEntry.kt
@@ -3,11 +3,11 @@ package com.example.jourlyapp.model.journal.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.jourlyapp.model.journal.enums.Mood
-import java.util.Date
+import java.time.LocalDateTime
 
 @Entity(tableName = "journal_entry")
 class JournalEntry(
     @PrimaryKey val id: Int,
-    val date: Date,
+    val date: LocalDateTime,
     var mood: Mood = Mood.None,
 )
