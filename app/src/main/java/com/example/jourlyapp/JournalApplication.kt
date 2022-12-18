2512de279da1a1b96be1e477be96886832fdb99c
Christopher Nagy <chrisn4gy@gmail.com>
2022-12-18 15:59:13 +0100

    docs: add doc comments to JournalApplication.kt

--- app/src/main/java/com/example/jourlyapp/JournalApplication.kt
+++ app/src/main/java/com/example/jourlyapp/JournalApplication.kt
@@ -5,7 +5,11 @@ import com.example.jourlyapp.model.journal.db.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
-// Wrapper for the database so that it is only created when needed (see https://developer.android.com/codelabs/android-room-with-a-view-kotlin#12)
+/**
+ * Wrapper for the database so that it is only created when needed. This means accessing the database through the
+ * [database] reference should be the only way!
+ * See: https://developer.android.com/codelabs/android-room-with-a-view-kotlin#12
+ */
 class JournalApplication : Application() {
     val applicationScope = CoroutineScope(SupervisorJob())
 
