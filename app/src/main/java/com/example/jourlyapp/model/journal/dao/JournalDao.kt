321e7017cc34e5a98435bf97e8c1435034fb801f
Christopher Nagy <chrisn4gy@gmail.com>
2022-12-18 15:32:59 +0100

    feat: add needed functions to JournalDao.kt

--- app/src/main/java/com/example/jourlyapp/model/journal/dao/JournalDao.kt
+++ app/src/main/java/com/example/jourlyapp/model/journal/dao/JournalDao.kt
@@ -7,27 +7,32 @@ import com.example.jourlyapp.model.journal.entities.JournalEntry
 import com.example.jourlyapp.model.journal.entities.QuestionAnswerPair
 import kotlinx.coroutines.flow.Flow
 
-// For accessing journal entries in local DB
-// https://developer.android.com/training/data-storage/room/accessing-data
+/**
+ * Interface for simple access of journal entries in local DB.
+ * Also see: https://developer.android.com/training/data-storage/room/accessing-data
+ */
 @Dao
 interface JournalDao {
     @Insert
-    suspend fun insertEntries(entries: List<JournalEntry>)
-
-    @Insert
-    suspend fun insertQuestionAnswerPairs(questionAnswerPairs: List<QuestionAnswerPair>)
+    suspend fun insertEntry(entry: JournalEntry)
 
     @Query("SELECT * FROM journal_entry ORDER BY date DESC")
     fun getEntries(): Flow<List<JournalEntry>>
 
-    @Query(
-        "SELECT qa.question, qa.answer FROM journal_entry " +
-                "JOIN question_answer_pair as qa ON journal_entry.id = qa.journal_entry_id AND journal_entry_id = :journalEntryId"
-    )
-    fun getQuestionnaireByEntryId(journalEntryId: Int): List<QuestionAnswerPair>
+    @Query("DELETE FROM journal_entry WHERE id = :journalEntryId")
+    suspend fun deleteEntryById(journalEntryId: Int)
 
     @Query("DELETE FROM journal_entry")
     suspend fun deleteAllEntries()
+
+    @Insert
+    suspend fun insertQuestionAnswerPair(questionAnswerPair: QuestionAnswerPair)
+
+    @Query(
+        "SELECT qa.id, qa.journal_entry_id, qa.question, qa.answer FROM journal_entry " +
+                "JOIN question_answer_pair as qa ON journal_entry.id = qa.journal_entry_id AND journal_entry_id = :journalEntryId"
+    )
+    fun getQuestionnaireByEntryId(journalEntryId: Int): List<QuestionAnswerPair>
 
     @Query("DELETE FROM question_answer_pair")
     suspend fun deleteAllQuestionAnswerPairs()
