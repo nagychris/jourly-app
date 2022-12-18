b56ab1a54b3dff3596a4e3806a1077b1b9a4c602
Christopher Nagy <chrisn4gy@gmail.com>
2022-12-18 15:48:01 +0100

    docs: change to KDoc comment

--- app/src/main/java/com/example/jourlyapp/model/journal/db/Converters.kt
+++ app/src/main/java/com/example/jourlyapp/model/journal/db/Converters.kt
@@ -3,8 +3,10 @@ package com.example.jourlyapp.model.journal.db
 import androidx.room.TypeConverter
 import java.time.LocalDateTime
 
-// Provides type converters for storing complex objects (e.g. Date) in the Room DB
-// https://developer.android.com/training/data-storage/room/referencing-data#type-converters
+/**
+ * Provides type converters for storing complex objects (e.g. Date) in the Room DB
+ * See: https://developer.android.com/training/data-storage/room/referencing-data#type-converters
+ */
 class Converters {
 
     @TypeConverter
