a68352e96f92177f2aa14d851a5c13f57fe19668
Diego <diego.martin@mail.polimi.it>
2023-03-19 17:05:14 +0100

    First step for the modal functionality. As for now the modal is shown upon clicking on the '+' button on the bottom bar. Still has some troubles with showing the mood emojis.

--- app/src/main/java/com/example/jourlyapp/ui/components/journal/AddEntryFAB.kt
+++ app/src/main/java/com/example/jourlyapp/ui/components/journal/AddEntryFAB.kt
@@ -2,16 +2,25 @@ package com.example.jourlyapp.ui.components.journal
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import com.example.jourlyapp.R
 import com.example.jourlyapp.ui.theme.JourlyTheme
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -47,6 +56,18 @@ fun AddEntryFAB() {
             }
         }
     }
+
+    // If the value of isToggled is "true", it opens the dialog by calling the BuildEntryModal function
+    if (isToggled) {
+        BuildEntryModal(
+            onMood1Click = { /*TODO*/ },
+            onMood2Click = { /*TODO*/ },
+            onMood3Click = { /*TODO*/ },
+            onMood4Click = { /*TODO*/ },
+            onMood5Click = { /*TODO*/ },
+            onArrowClick = { /*TODO*/ },
+            onDismissRequest = {isToggled = !isToggled})
+    }
 }
 
 @Preview
@@ -54,5 +75,108 @@ fun AddEntryFAB() {
 fun AddEntryFABPreview() {
     JourlyTheme {
         AddEntryFAB()
+    }
+}
+
+
+/**
+ * This function's goal is to build and show the modal that is used to add a quick entry of
+ * the user's mood.
+ */
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BuildEntryModal(
+    //title: String,
+    onMood1Click: () -> Unit,
+    onMood2Click: () -> Unit,
+    onMood3Click: () -> Unit,
+    onMood4Click: () -> Unit,
+    onMood5Click: () -> Unit,
+    onArrowClick: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+     AlertDialog(
+         onDismissRequest = onDismissRequest,
+         properties = DialogProperties(usePlatformDefaultWidth = false),
+         modifier = Modifier
+             .padding(28.dp)
+             .fillMaxWidth()
+             .wrapContentHeight()
+             .wrapContentWidth(),
+         buttons = {
+             Column(
+                 horizontalAlignment = Alignment.CenterHorizontally,
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .padding(horizontal = 16.dp)
+             ) {
+                 // This button could be moved out of the row
+                 IconButton(onClick = onArrowClick) {
+                     Icon(
+                         painterResource(id = R.drawable.ic_baseline_keyboard_arrow_up_24),
+                         contentDescription = "Button to detailed entry",
+                         modifier = Modifier.size(20.dp)
+                     )
+                 }
+
+                 Text(text = "What is your today's mood?", fontWeight = FontWeight.Bold)
+
+                 Row (
+                     Modifier
+                         .fillMaxWidth()
+                         .padding(vertical = 8.dp),
+                     horizontalArrangement = Arrangement.SpaceBetween,
+                     verticalAlignment = Alignment.CenterVertically,
+                 ){
+                     IconButton(onClick = onMood1Click) {
+                         Icon(
+                             painterResource(id = R.drawable.great_mood_emoji),
+                             contentDescription = "Button to add a quick entry of the mood: Great",
+                             modifier = Modifier.size(30.dp),
+                             tint = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+                         )
+                     }
+                     IconButton(onClick = onMood2Click) {
+                         Icon(
+                             painterResource(id = R.drawable.good_mood_emoji),
+                             contentDescription = "Button to add a quick entry of the mood: Good",
+                             modifier = Modifier.size(30.dp)
+                         )
+                     }
+                     IconButton(onClick = onMood3Click) {
+                         Icon(
+                             painterResource(id = R.drawable.okay_mood_emoji),
+                             contentDescription = "Button to add a quick entry of the mood: Okay",
+                             modifier = Modifier.size(30.dp)
+                         )
+                     }
+                     IconButton(onClick = onMood4Click) {
+                         Icon(
+                             painterResource(id = R.drawable.bad_mood_emoji),
+                             contentDescription = "Button to add a quick entry of the mood: Bad",
+                             modifier = Modifier.size(30.dp)
+                         )
+                     }
+                     IconButton(onClick = onMood5Click) {
+                         Icon(
+                             painterResource(id = R.drawable.awful_mood_emoji),
+                             contentDescription = "Button to add a quick entry of the mood: Awful",
+                             modifier = Modifier.size(30.dp)
+                         )
+                     }
+                 }
+             }
+         }
+     )
+}
+
+
+@Preview
+@Composable
+fun BuildEntryModalPreview() {
+    JourlyTheme {
+        BuildEntryModal( onDismissRequest = {}, onArrowClick = {},
+        onMood1Click = {}, onMood2Click = {}, onMood3Click = {}, onMood4Click = {},
+        onMood5Click = {})
     }
 }
