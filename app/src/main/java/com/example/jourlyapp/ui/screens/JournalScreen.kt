cef9d58ca05ec07f2d4a8e2ff253d52f7946c6e3
Christopher Nagy <chrisn4gy@gmail.com>
2023-03-26 21:55:39 +0200

    feat: use paddingValues

--- app/src/main/java/com/example/jourlyapp/ui/screens/JournalScreen.kt
+++ app/src/main/java/com/example/jourlyapp/ui/screens/JournalScreen.kt
@@ -1,9 +1,25 @@
 package com.example.jourlyapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun JournalScreen(modifier: Modifier) {
-    // TODO: implement entry list @CostyaC
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("To be implemented")
+        // TODO: implement entry list @CostyaC
+    }
 }
