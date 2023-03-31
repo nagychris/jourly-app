547c5e8d44d9243d98a83d5888158d2fe9a1b5e7
Diego <diego.martin@mail.polimi.it>
2023-03-31 11:33:01 +0200

    Fixed the insertion in the database, now the QuickEntryModal works as intended.

--- app/src/main/java/com/example/jourlyapp/viewmodel/QuickEntryModalViewModel.kt
+++ app/src/main/java/com/example/jourlyapp/viewmodel/QuickEntryModalViewModel.kt
@@ -3,12 +3,20 @@ package com.example.jourlyapp.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.jourlyapp.JourlyApplication
 import com.example.jourlyapp.model.journal.JournalRepository
+import com.example.jourlyapp.model.journal.entities.JournalEntry
+import com.example.jourlyapp.model.journal.enums.Mood
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
-class QuickEntryModalViewModel(journalRepository: JournalRepository): ViewModel() {
+class QuickEntryModalViewModel(private var journalRepository: JournalRepository): ViewModel() {
+    fun createNewQuickEntry(date : LocalDateTime, mood: Mood) = viewModelScope.launch {
+        journalRepository.createEntry(JournalEntry(null, date, mood))
+    }
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
