3463ce34ba1a74ccc56a854b5fae1d7eaf313ca0
Diego <diego.martin@mail.polimi.it>
2023-04-13 17:13:19 +0200

    Implementation of function for deleting entries

--- app/src/main/java/com/example/jourlyapp/viewmodel/journal/JournalViewModel.kt
+++ app/src/main/java/com/example/jourlyapp/viewmodel/journal/JournalViewModel.kt
@@ -2,16 +2,14 @@ package com.example.jourlyapp.viewmodel.journal
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.jourlyapp.JourlyApplication
 import com.example.jourlyapp.model.auth.UserRepository
 import com.example.jourlyapp.model.journal.JournalRepository
 import com.example.jourlyapp.model.journal.entities.JournalEntry
+import kotlinx.coroutines.launch
 
 class JournalViewModel(
     private val userRepository: UserRepository,
@@ -25,6 +23,10 @@ class JournalViewModel(
 
     init {
         userName.value = userRepository.getUser()?.name ?: ""
+    }
+
+    fun deleteEntry(entryId : Int) = viewModelScope.launch{
+        journalRepository.deleteEntryById(entryId)
     }
 
     companion object {
