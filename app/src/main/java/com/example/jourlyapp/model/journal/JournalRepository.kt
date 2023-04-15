edfab7a713833aba648a33f447d90a885e3a5603
Diego <diego.martin@mail.polimi.it>
2023-04-15 20:24:42 +0200

    Implemented functionality for the detailed journal entry.

--- app/src/main/java/com/example/jourlyapp/model/journal/JournalRepository.kt
+++ app/src/main/java/com/example/jourlyapp/model/journal/JournalRepository.kt
@@ -2,6 +2,7 @@ package com.example.jourlyapp.model.journal
 
 import com.example.jourlyapp.model.journal.dao.JournalDao
 import com.example.jourlyapp.model.journal.entities.JournalEntry
+import com.example.jourlyapp.model.journal.entities.QuestionAnswerPair
 import kotlinx.coroutines.flow.Flow
 import java.time.LocalDateTime
 
@@ -20,6 +21,11 @@ class JournalRepository(private val journalDao: JournalDao) {
     }
 
     suspend fun createEntry(journalEntry: JournalEntry) = journalDao.insertEntry(journalEntry)
+
+    suspend fun createQuestionAnswerPair(questionAnswerPair: QuestionAnswerPair) =
+        journalDao.insertQuestionAnswerPair(questionAnswerPair)
     suspend fun deleteEntryById (entryId: Int) = journalDao.deleteEntryById(journalEntryId = entryId)
+
+    fun getLastEntryId () : Int? = journalDao.getLastEntry().id
 
 }
