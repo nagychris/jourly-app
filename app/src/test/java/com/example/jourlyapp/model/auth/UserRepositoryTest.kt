37e865fb47f20750515dbcea3cb999facf5e6b55
Christopher Nagy <chrisn4gy@gmail.com>
2023-04-16 17:10:20 +0200

    refactor: change to UserRepositoryTest.kt

--- app/src/test/java/com/example/jourlyapp/model/auth/UserRepositoryTest.kt
+++ app/src/test/java/com/example/jourlyapp/model/auth/UserRepositoryTest.kt
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class UserRepositoryImplTest {
+class UserRepositoryTest {
 
     private val mockStorageService =
         mockk<EncryptedStorageService>(relaxed = true)
